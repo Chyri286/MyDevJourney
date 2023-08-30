eab5162d5c98c35bf8cf50d24a00a8e7acb433d2
Chyri286 <87230894+Chyri286@users.noreply.github.com>
2023-08-30 15:00:28 -0400

    Add files via upload

---
+++ src/BankingApplication.java
@@ -0,0 +1,11 @@
+package Banking;
+
+public class BankingApplication 
+{
+	public static void main(String[] args)
+	{
+		BankAccount object1 = new BankAccount("CIBC", "00014286");
+		object1.showMenu();
+	}
+
+}
